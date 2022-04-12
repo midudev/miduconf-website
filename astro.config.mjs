4def0fe804bd96976e448b31ccaffd5f6eae4f08
David Ponce Vargas <buap.david@gmail.com>
2022-04-12 09:46:46 -0500

    chore: config and define of custom colors

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind({ config: { applyAstroPreset: false, applyBaseStyles: true } })]
 });
