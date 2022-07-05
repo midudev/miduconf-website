604ec14d5be63a9dadab08769d1f7565ff541c28
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-07-05 01:30:11 -0400

    remove default tailwind config  and deprecated preset

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ config: { applyAstroPreset: false, applyBaseStyles: true } })]
+  integrations: [tailwind()]
 });
