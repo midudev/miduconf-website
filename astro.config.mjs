50359b9ef2d2387a109646481932f8708bfa91b1
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-07-05 01:38:46 -0400

    format code with prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ config: { applyAstroPreset: false, applyBaseStyles: true } })]
+  integrations: [
+    tailwind({ config: { applyAstroPreset: false, applyBaseStyles: true } }),
+  ],
 });
