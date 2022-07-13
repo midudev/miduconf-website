519b61bd31e74371627e9f65ca76e84e4c237ee7
Freddy González <7684330+fredoist@users.noreply.github.com>
2022-07-13 15:55:02 -0500

    Fix build error preventing calendar link to be externalized for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,10 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  vite: {
+    ssr: {
+      noExternal: ['calendar-link']
+    }
+  }
 });
