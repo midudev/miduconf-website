21da87d5434a8166b61d8c464ee48c4301e76aad
Miguel Ángel Durán <miduga@gmail.com>
2022-07-30 21:02:21 +0200

    Add Netlify functions

---
+++ functions/image.js
@@ -0,0 +1,51 @@
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
+
+const isLocal = process.env.NETLIFY_LOCAL === 'true';
+
+const LOCAL_CHROME_PATH = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+const LOCAL_URL = 'http://localhost:3000/ticket?username=';
+
+const getConfig = async () => {
+	const executablePath = isLocal ? LOCAL_CHROME_PATH : await chromium.executablePath();
+	const url = isLocal ? LOCAL_URL : 'https://miduconf.com/ticket?username=';
+
+	return { executablePath, url };
+};
+
+exports.handler = async function (event) {
+	const {
+		queryStringParameters: { username },
+	} = event;
+	const { executablePath, url } = await getConfig();
+
+	const browser = await puppeteer.launch({
+		executablePath,
+		headless: true,
+	});
+
+	const page = await browser.newPage();
+	await page.setViewport({
+		width: 1200,
+		height: 800,
+		deviceScaleFactor: 2,
+	});
+
+	await page.goto(`${url}${username}`);
+
+	await page.waitForSelector('.atropos[data-ready]');
+	const el = await page.$('.atropos');
+
+	const screenshot = await el.screenshot({ path: './image.png' });
+
+	await browser.close();
+
+	return {
+		headers: {
+			'Content-Type': 'image/png',
+		},
+		statusCode: 200,
+		body: screenshot.toString('base64'),
+		isBase64Encoded: true,
+	};
+};
