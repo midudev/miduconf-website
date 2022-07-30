9df2d74a01b8e8b018c83c2be94452359018a532
Miguel Ángel Durán <miduga@gmail.com>
2022-07-30 21:01:43 +0200

    Add Preact support to Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	integrations: [tailwind(), preact()],
 });
