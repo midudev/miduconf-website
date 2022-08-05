1a8d6a477409c61b3ba9b806abfbcd33ce0e24d5
Miguel Ángel Durán <miduga@gmail.com>
2022-08-05 13:01:10 +0200

    Add number of tickets

---
+++ src/components/TicketsSold.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from "preact/hooks"
+import { useProgressiveNumber } from "@hooks/useProgressiveNumber"
+import { supabase } from "src/lib/supabase.js"
+
+export const TicketsSold = () => {
+  const [tickets, setTickets] = useProgressiveNumber(0)
+
+  useEffect(() => {
+    async function fetchTickets() {
+      const { count } = await supabase.from("ticket").select('*', { count: 'exact', head: true })
+      setTickets(count)
+    }
+    fetchTickets()
+  }, [])
+
+  return (
+    <strong class="text-gray-700 text-center text-xl flex items-center justify-center space-x-4 my-2">
+      ¡Ya se han repartido <span class="text-4xl font-black bg-clip-text text-transparent mx-1 bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">{tickets.toFixed(0).toString()}</span> tickets! 
+    </strong>
+  )
+    
+}
