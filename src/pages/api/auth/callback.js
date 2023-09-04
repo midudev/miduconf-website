978160a0270f1a100c6bffe2502233719edbea37
Miguel Ángel Durán <miduga@gmail.com>
2023-09-04 20:02:06 +0200

    Add console.error

--- src/pages/api/auth/callback.js
+++ src/pages/api/auth/callback.js
@@ -1,14 +1,18 @@
 import { createPagesServerClient } from '@supabase/auth-helpers-nextjs'
 
 const handler = async (req, res) => {
-	const { code } = req.query
+	try {
+		const { code } = req.query
 
-	if (code) {
-		const supabase = createPagesServerClient({ req, res })
-		await supabase.auth.exchangeCodeForSession(String(code))
+		if (code) {
+			const supabase = createPagesServerClient({ req, res })
+			await supabase.auth.exchangeCodeForSession(String(code))
+		}
+
+		res.redirect('/ticket')
+	} catch (e) {
+		console.error(e)
 	}
-
-	res.redirect('/ticket')
 }
 
 export default handler
