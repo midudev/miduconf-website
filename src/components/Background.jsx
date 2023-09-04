7eaea69f91b7244609c3a43121dc1f7c10c4ec34
Daniel Mondragón <110652280+danmondra@users.noreply.github.com>
2023-09-04 11:01:22 -0600

    Fix overflow from background svg

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,7 +3,7 @@ export const Background = () => (
 		<div className='pointer-events-none absolute inset-0 h-screen bg-[radial-gradient(ellipse_80%_50%_at_50%_-20%,rgba(120,119,198,0.2),rgba(255,255,255,0))] dark:bg-[radial-gradient(ellipse_80%_50%_at_50%_-20%,rgba(120,119,198,0.2),rgba(255,255,255,0))]'></div>
 		<svg
 			aria-hidden='true'
-			className='pointer-events-none absolute inset-0 w-screen fill-black/[.06] stroke-black/[.06] dark:fill-white/[.06] dark:stroke-white/[.06] [mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] inset-x-0 -top-[1rem] -z-50 h-[80%] skew-y-12'
+			className='pointer-events-none max-w-full	absolute inset-0 w-screen fill-black/[.06] stroke-black/[.06] dark:fill-white/[.06] dark:stroke-white/[.06] [mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] inset-x-0 -top-[1rem] -z-50 h-[80%] skew-y-12'
 		>
 			<defs>
 				<pattern id=':S1:' width='80' height='80' patternUnits='userSpaceOnUse' x='-1' y='-1'>
