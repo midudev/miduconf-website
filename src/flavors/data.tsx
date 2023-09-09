d8d001141646326669226387f124b95fe87c621e
Juan sebastian <122684235+GuauDev@users.noreply.github.com>
2023-09-09 18:10:48 +0000

    ADD tailwind flavor

--- src/flavors/data.tsx
+++ src/flavors/data.tsx
@@ -1391,5 +1391,19 @@ export const FLAVORS = {
 			month: 'text-[#FCC624]',
 			time: 'text-[#F8BF11]'
 		}
-	}
+	},
+	tailwind: {
+		component: ({ className }) => (
+			<svg xmlns="http://www.w3.org/2000/svg" className={className} viewBox="0 0 32 32" height="auto" fill="#44a8b3">
+				<path d="M9,13.7q1.4-5.6,7-5.6c5.6,0,6.3,4.2,9.1,4.9q2.8.7,4.9-2.1-1.4,5.6-7,5.6c-5.6,0-6.3-4.2-9.1-4.9Q11.1,10.9,9,13.7ZM2,22.1q1.4-5.6,7-5.6c5.6,0,6.3,4.2,9.1,4.9q2.8.7,4.9-2.1-1.4,5.6-7,5.6c-5.6,0-6.3-4.2-9.1-4.9Q4.1,19.3,2,22.1Z" />
+			</svg>
+		),
+		colors: {
+			ticket: 'bg-[#44a8b3]/30',
+			border: 'border-[#ffffff]/60',
+			text: 'text-[#ffffff]',
+			month: 'text-[#ffffff]',
+			time: 'text-[#ffffff]'
+		}
+	},
 }
