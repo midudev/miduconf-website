fd1a706f8213dbd01f5018a720c2e3e24afaa4dc
madeval <alvarez.fing@gmail.com>
2024-08-02 01:33:37 -0300

    in progress: improve style of title

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,14 +51,14 @@ export default function Home({ username, flavor, ticketNumber, burst }) {
 			<Header />
 
 			{/* <HeaderIndex /> */}
-			<div class='text-4xl md:text-8xl mx-auto text-center max-w-[20ch] text-white font-semibold'>
-				<p>
-					Conoce el <span className='text-blue-600'>futuro</span> del{' '}
-					<span className='text-blue-600'>desarrollo</span> web
-				</p>
-			</div>
 
 			<main className={`${inter.className} max-w-5xl m-auto mt-16 pb-20 px-4`}>
+				<div class='text-6xl md:text-8xl mx-auto text-center max-w-[20ch] text-white font-bold mt-40'>
+					<p>
+						Conoce el <span className='text-blue-600'>futuro</span> del{' '}
+						<span className='text-blue-600'>desarrollo</span> web
+					</p>
+				</div>
 				<TicketHome ticketNumber={ticketNumber} initialFlavor={flavor} username={username} />
 				<Speakers />
 				<Sponsors />
