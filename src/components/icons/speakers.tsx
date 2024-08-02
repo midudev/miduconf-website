ec6f8f2ae6e768a8ff37b702bd4f04df4d07c676
Miguel Ángel Durán <miduga@gmail.com>
2024-08-02 11:15:00 +0200

    Use correct prop

--- src/components/icons/speakers.tsx
+++ src/components/icons/speakers.tsx
@@ -5,32 +5,32 @@ export function SpeakersIcon({ className = '', ...props }) {
 				<path
 					d='M5 7C5 8.06087 5.42143 9.07828 6.17157 9.82843C6.92172 10.5786 7.93913 11 9 11C10.0609 11 11.0783 10.5786 11.8284 9.82843C12.5786 9.07828 13 8.06087 13 7C13 5.93913 12.5786 4.92172 11.8284 4.17157C11.0783 3.42143 10.0609 3 9 3C7.93913 3 6.92172 3.42143 6.17157 4.17157C5.42143 4.92172 5 5.93913 5 7Z'
 					stroke='currentColor'
-					stroke-width='1.5'
-					stroke-linecap='round'
-					stroke-linejoin='round'
+					strokeWidth='1.5'
+					strokeLinecap='round'
+					strokeLinejoin='round'
 					className={className}
 					{...props}
 				/>
 				<path
 					d='M3 21V19C3 17.9391 3.42143 16.9217 4.17157 16.1716C4.92172 15.4214 5.93913 15 7 15H11C12.0609 15 13.0783 15.4214 13.8284 16.1716C14.5786 16.9217 15 17.9391 15 19V21'
 					stroke='currentColor'
-					stroke-width='1.5'
-					stroke-linecap='round'
-					stroke-linejoin='round'
+					strokeWidth='1.5'
+					strokeLinecap='round'
+					strokeLinejoin='round'
 				/>
 				<path
 					d='M16 3.12988C16.8604 3.35018 17.623 3.85058 18.1676 4.55219C18.7122 5.2538 19.0078 6.11671 19.0078 7.00488C19.0078 7.89305 18.7122 8.75596 18.1676 9.45757C17.623 10.1592 16.8604 10.6596 16 10.8799'
 					stroke='currentColor'
-					stroke-width='1.5'
-					stroke-linecap='round'
-					stroke-linejoin='round'
+					strokeWidth='1.5'
+					strokeLinecap='round'
+					strokeLinejoin='round'
 				/>
 				<path
 					d='M21 20.9999V18.9999C20.9949 18.1171 20.6979 17.2607 20.1553 16.5643C19.6126 15.8679 18.8548 15.3706 18 15.1499'
 					stroke='currentColor'
-					stroke-width='1.5'
-					stroke-linecap='round'
-					stroke-linejoin='round'
+					strokeWidth='1.5'
+					strokeLinecap='round'
+					strokeLinejoin='round'
 				/>
 			</g>
 			<defs>
