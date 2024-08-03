d61d381aa55fb1b821534c4153ef0c9aacb92db8
madeval <alvarez.fing@gmail.com>
2024-08-03 00:49:27 -0300

    feat: chage header of backgorund

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,6 +1,6 @@
 export const Background = () => (
 	<>
-		<div className='pointer-events-none absolute inset-0 h-screen bg-[radial-gradient(ellipse_80%_50%_at_50%_-20%,rgba(120,119,198,0.2),rgba(255,255,255,0))] dark:bg-[radial-gradient(ellipse_80%_50%_at_50%_-20%,rgba(120,119,198,0.2),rgba(255,255,255,0))]'></div>
+		<div className='pointer-events-none absolute inset-0 h-full bg-[radial-gradient(ellipse_80%_50%_at_50%_-20%,rgba(120,119,198,0.2),rgba(255,255,255,0))] dark:bg-[radial-gradient(ellipse_80%_50%_at_50%_-20%,rgba(120,119,198,0.2),rgba(255,255,255,0))]'></div>
 		<svg
 			aria-hidden='true'
 			className='pointer-events-none w-full absolute inset-0 fill-black/[.06] stroke-black/[.06] dark:fill-white/[.06] dark:stroke-white/[.06] [mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] inset-x-0 -top-[1rem] -z-50 h-[80%] skew-y-12'
