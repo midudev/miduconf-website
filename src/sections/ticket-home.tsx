02de00bf682b6c9a69451557bd0b2b719bd1a2aa
Miguel Ángel Durán <miduga@gmail.com>
2024-08-04 12:47:26 +0200

    Remove not needed content

--- src/sections/ticket-home.tsx
+++ src/sections/ticket-home.tsx
@@ -76,54 +76,6 @@ export const TicketHome = ({ ticketNumber, username, initialFlavor }) => {
 						<TicketIcon className='mr-3' />
 						Conseguir mi ticket
 					</Button>
-					{/* 	<ShimmerButton onClick={handleLogin} size='large'>
-						<svg
-							className='mr-3'
-							width='32'
-							height='32'
-							viewBox='0 0 24 24'
-							strokeWidth='1'
-							stroke='currentColor'
-							fill='none'
-							strokeLinecap='round'
-							strokeLinejoin='round'
-						>
-							<path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
-							<path d='M15 5l0 2'></path>
-							<path d='M15 11l0 2'></path>
-							<path d='M15 17l0 2'></path>
-							<path d='M5 5h14a2 2 0 0 1 2 2v3a2 2 0 0 0 0 4v3a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-3a2 2 0 0 0 0 -4v-3a2 2 0 0 1 2 -2'></path>
-						</svg>
-						¡Consigue tu entrada GRATIS!
-					</ShimmerButton>
-					<ShimmerButton
-						shimmerDuration='0'
-						shimmerSize='0'
-						type='secondary'
-						onClick={handleGoToDiscord}
-						size='large'
-					>
-						<svg
-							xmlns='http://www.w3.org/2000/svg'
-							className='mr-3'
-							width='32'
-							height='32'
-							viewBox='0 0 24 24'
-							strokeWidth='1.5'
-							stroke='currentColor'
-							fill='none'
-							strokeLinecap='round'
-							strokeLinejoin='round'
-						>
-							<path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
-							<path d='M11.5 21h-5.5a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v6'></path>
-							<path d='M16 3v4'></path>
-							<path d='M8 3v4'></path>
-							<path d='M4 11h16'></path>
-							<path d='M15 19l2 2l4 -4'></path>
-						</svg>
-						Guarda la fecha del evento
-					</ShimmerButton> */}
 				</div>
 			</div>
 		</div>
