c12fdf137e01749eb4ea7c47f50966fb0d12a7c8
madeval <alvarez.fing@gmail.com>
2024-08-06 17:25:00 -0300

    feat: change cursor in gift section

--- src/sections/gifts.tsx
+++ src/sections/gifts.tsx
@@ -91,7 +91,7 @@ const GIFTS = [
 export const Gifts = () => {
 	return (
 		<section id='regalos' className='px-4 pt-24'>
-			<div className='flex relative group flex-col gap-4 py-20 mx-auto shadow-[inset_0_4px_30px] shadow-midu-primary/25 overflow-hidden rounded-2xl bg-midu-primary/10 max-w-screen-base'>
+			<div className='flex relative group flex-col cursor-crosshair gap-4 py-20 mx-auto shadow-[inset_0_4px_30px] shadow-midu-primary/25 overflow-hidden rounded-2xl bg-midu-primary/10 max-w-screen-base'>
 				<div className='pointer-events-none absolute w-1/2 rotate-45 h-[300%] left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 bg-gradient-to-r from-[#41b3ff00] via-[#b0a9ff13] to-[#41b3ff00]'></div>
 				<h2 className='text-6xl font-bold text-center text-white'>Regalos</h2>
 				<p className='text-4xl font-semibold text-center max-w-[24ch] text-yellow-200 mx-auto px-4'>
