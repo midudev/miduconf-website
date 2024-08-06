7ff3b84a6c558dbd6584e32680b056876744e8ef
madeval <alvarez.fing@gmail.com>
2024-08-05 23:59:52 -0300

    feat: add new icons with border as sticker

---
+++ src/components/icons/languages/next.tsx
@@ -0,0 +1,65 @@
+export function NextIcon({ className = '', ...props }) {
+	return (
+		<svg
+			width='661'
+			height='660'
+			viewBox='0 0 661 660'
+			fill='none'
+			className={className}
+			{...props}
+			xmlns='http://www.w3.org/2000/svg'
+		>
+			<mask
+				id='mask0_7_173'
+				style={{ maskType: 'alpha' }}
+				maskUnits='userSpaceOnUse'
+				x='0'
+				y='0'
+				width='661'
+				height='660'
+			>
+				<path
+					d='M330.12 660C512.374 660 660.12 512.254 660.12 330C660.12 147.746 512.374 0 330.12 0C147.866 0 0.120117 147.746 0.120117 330C0.120117 512.254 147.866 660 330.12 660Z'
+					fill='black'
+				/>
+			</mask>
+			<g mask='url(#mask0_7_173)'>
+				<path
+					d='M330.12 649C506.299 649 649.12 506.179 649.12 330C649.12 153.821 506.299 11 330.12 11C153.941 11 11.1201 153.821 11.1201 330C11.1201 506.179 153.941 649 330.12 649Z'
+					fill='black'
+					stroke='white'
+					stroke-width='25.2'
+				/>
+				<path
+					d='M548.316 577.573L253.641 198H198.12V461.89H242.537V254.407L513.45 604.432C525.674 596.251 537.32 587.272 548.316 577.573Z'
+					fill='url(#paint0_linear_7_173)'
+				/>
+				<path d='M465.787 198H421.787V462H465.787V198Z' fill='url(#paint1_linear_7_173)' />
+			</g>
+			<defs>
+				<linearGradient
+					id='paint0_linear_7_173'
+					x1='399.787'
+					y1='427.167'
+					x2='529.953'
+					y2='588.5'
+					gradientUnits='userSpaceOnUse'
+				>
+					<stop stop-color='white' />
+					<stop offset='1' stop-color='white' stop-opacity='0' />
+				</linearGradient>
+				<linearGradient
+					id='paint1_linear_7_173'
+					x1='443.787'
+					y1='198'
+					x2='443.05'
+					y2='391.875'
+					gradientUnits='userSpaceOnUse'
+				>
+					<stop stop-color='white' />
+					<stop offset='1' stop-color='white' stop-opacity='0' />
+				</linearGradient>
+			</defs>
+		</svg>
+	)
+}
