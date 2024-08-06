8f75812d54a4c7a31ef389c67e4bdd95681ff95e
madeval <alvarez.fing@gmail.com>
2024-08-06 03:13:25 -0300

    feat: set new ticket aspect radio to mobile

--- src/components/Container3D.tsx
+++ src/components/Container3D.tsx
@@ -6,13 +6,13 @@ interface Props {
 
 export const Container3D = ({ children }: Props) => {
 	return (
-		<div className='relative z-[1000] w-full h-auto mx-auto aspect-[9/10] md:aspect-[2/1]'>
-			<div className='h-full opacity-100 isolate aspect-[9/10] md:aspect-[2/1]'>
+		<div className='relative z-[1000] w-full h-auto mx-auto md:aspect-[2/1]'>
+			<div className='h-full opacity-100 isolate md:aspect-[2/1]'>
 				<div className='h-full'>
 					<Atropos
 						highlight={false}
 						innerClassName='backdrop-blur-xl rounded-[60px]'
-						className='block w-full h-auto mx-auto shadow-2xl aspect-[9/10] md:aspect-[2/1] [box-sizing:border-box]'
+						className='block w-full h-auto mx-auto shadow-2xl md:aspect-[2/1] [box-sizing:border-box]'
 					>
 						{children}
 					</Atropos>
