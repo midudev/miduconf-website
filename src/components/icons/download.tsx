61a15fde84d9b2136242da165b7120769c322e0b
Angel Gabriel Lopez Solano <alopez@dextra.com.do>
2024-08-09 10:13:40 -0400

    fix: Fix stroke-width attribute in DownloadIcon component

--- src/components/icons/download.tsx
+++ src/components/icons/download.tsx
@@ -1,4 +1,4 @@
 import { cn } from '@/lib/utils'
 export const DownloadIcon: React.FC<React.SVGProps<SVGSVGElement>> = ({className, ...props}) => (
-	<svg className={cn('size-6', className)} {...props}  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round" ><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" /><path d="M7 11l5 5l5 -5" /><path d="M12 4l0 12" /></svg>
+	<svg className={cn('size-6', className)} {...props}  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  stroke-linecap="round"  strokeLinejoin="round" ><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" /><path d="M7 11l5 5l5 -5" /><path d="M12 4l0 12" /></svg>
 ) 
