7765afa6fcafe48767ee61d57c436e7d519dab66
madeval <alvarez.fing@gmail.com>
2024-08-12 17:43:23 -0300

    feat: new ticket page structure

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,20 +5,29 @@ type Props<C extends React.ElementType> = {
 	as?: C
 	children: React.ReactNode
 	className?: string
+	variant?: 'primary' | 'secondary'
 } & ComponentPropsWithoutRef<C>
 
 export const Button = <C extends React.ElementType = 'button'>({
 	as,
 	children,
+	variant = 'primary',
 	...restOfProps
 }: Props<C>) => {
 	const As = as ?? 'button'
+
+	const variantStyle = {
+		primary: 'bg-button text-white shadow-button hover:shadow-button-hover hover:scale-110 ',
+		secondary:
+			'border border-midu-primary/40 bg-[#121226] hover:bg-[#1A1A2E] hover:border-midu-primary/60'
+	}
 
 	return (
 		<As
 			{...restOfProps}
 			className={cn(
-				'flex items-center gap-2 rounded-lg text-white px-3 py-[10px] bg-button shadow-button hover:shadow-button-hover hover:scale-110 transition-all duration-300',
+				'flex items-center gap-2 rounded-lg text-white px-3 py-[10px] transition-all duration-300 disabled:opacity-50 disabled:cursor-not-allowed',
+				variantStyle[variant],
 				restOfProps.className
 			)}
 		>
--- src/pages/ticket.js
+++ src/pages/ticket.js
@@ -8,36 +8,23 @@ import { createPagesServerClient } from '@supabase/auth-helpers-nextjs'
 import { useSupabaseClient } from '@supabase/auth-helpers-react'
 
 import { Background } from '@/components/Background'
+import { Button } from '@/components/Button'
 import { Container3D } from '@/components/Container3D'
 import { Meteors } from '@/components/MeteorLanguages'
 import { Stars } from '@/components/Stars'
 import TicketComponent from '@/components/Ticket'
 import { FLAVORS } from '@/flavors/data.tsx'
+import { cn } from '@/lib/utils'
 
-const PREFIX_CDN = 'https://ljizvfycxyxnupniyyxb.supabase.co/storage/v1/object/public/tickets'
-
-async function dataUrlToFile(dataUrl, fileName) {
-	const res = await fetch(dataUrl)
-	const blob = await res.blob()
-	return new File([blob], fileName, { type: 'image/jpg' })
-}
-
-const STEPS_LOADING = {
-	ready: 'Compartir ticket en X',
-	generate: 'Generando nuevo ticket...',
-	sharing: 'Compartiendo ticket...'
-}
-
-const getInfoFromUser = ({ user }) => {
-	const { user_metadata: meta } = user
-	const { avatar_url: avatar, full_name: fullname, preferred_username: username } = meta
-
-	return { avatar, fullname, username }
+const MATERIALS_AVAILABLE = {
+	STANDARD: 'standard',
+	GRANITE: 'granite',
+	PLATINUM: 'platinum'
 }
 
 export default function Ticket({ user, ticketNumber, selectedFlavor = 'javascript' }) {
 	const [buttonText, setButtonText] = useState(STEPS_LOADING.ready)
-	const [number, setNumber] = useState(ticketNumber)
+	const [selectedMaterial, setSelectedMaterial] = useState(MATERIALS_AVAILABLE.STANDARD)
 	const [flavorKey, setFlavorKey] = useState(() => {
 		// check selectedFlavor is valid
 		if (Object.keys(FLAVORS).includes(selectedFlavor)) {
@@ -55,7 +42,7 @@ export default function Ticket({ user, ticketNumber, selectedFlavor = 'javascrip
 		'¡No te pierdas la miduConf el 13 de SEPTIEMBRE! Charlas para todos los niveles, +256 regalos y premios, ¡y muchas sorpresas!'
 	const hash = crypto.randomUUID().split('-')[0]
 	const url = `https://miduconf.com/ticket/${username}/${hash}`
-	const ogImage = `${PREFIX_CDN}/ticket-${number}.jpg?${hash}=_buster`
+	const ogImage = `${PREFIX_CDN}/ticket-${ticketNumber}.jpg?${hash}=_buster`
 
 	const handleShare = async () => {
 		const intent = 'https://twitter.com/intent/tweet'
@@ -94,7 +81,7 @@ Apunta la fecha: 12 de SEPTIEMBRE
 		})
 
 		const file = await dataUrlToFile(dataURL, 'ticket.jpg')
-		const filename = `ticket-${number}.jpg`
+		const filename = `ticket-${ticketNumber}.jpg`
 
 		const { data: dataStorage, error: errorStorage } = await supabase.storage
 			.from('tickets')
@@ -126,101 +113,147 @@ Apunta la fecha: 12 de SEPTIEMBRE
 			<Stars />
 			<Meteors />
 			<Background />
-
-			<main className={`${GeistSans.className} max-w-screen-base m-auto mt-40 pb-20 px-4`}>
-				<div className='flex flex-col items-center justify-between w-full px-16 m-auto mt-16 mb-16 text-center md:flex-row'>
-					<button
-						onClick={handleShare}
-						type='button'
-						className={`text-white cursor-pointer hover:bg- bg-[#14171A] hover:bg-[#14171A]/90 focus:ring-4 focus:outline-none focus:ring-[#14171A]/50 font-medium rounded-lg px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#14171A]/55 mr-2 mb-2 hover:shadow-lg transition-all duration-200 ease-in-out border border-white/40 hover:scale-125 [text-wrap:balance] ${
-							buttonText !== STEPS_LOADING.ready
-								? 'pointer-events-none opacity-70 bg-slate-700'
-								: ''
-						}`}
-					>
-						<svg
-							xmlns='http://www.w3.org/2000/svg'
-							className='w-4 h-4 mr-2'
-							width='1200'
-							height='1227'
-							fill='none'
-							viewBox='0 0 1200 1227'
-						>
-							<path
-								fill='#fff'
-								d='M714.163 519.284 1160.89 0h-105.86L667.137 450.887 357.328 0H0l468.492 681.821L0 1226.37h105.866l409.625-476.152 327.181 476.152H1200L714.137 519.284h.026ZM569.165 687.828l-47.468-67.894-377.686-540.24h162.604l304.797 435.991 47.468 67.894 396.2 566.721H892.476L569.165 687.854v-.026Z'
-							/>
-						</svg>
-						{buttonText}
-					</button>
-					<button
-						onClick={handleLogout}
-						className='flex-row justify-center  text-white cursor-pointer hover:bg-slate-700 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55 mr-2 mb-2 hover:shadow-lg transition-all duration-200 ease-in-out hover:scale-110 scale-90 gap-x-2 opacity-70 hover:opacity-100'
-						href='/'
-					>
-						<svg
-							xmlns='http://www.w3.org/2000/svg'
-							width='24'
-							height='24'
-							viewBox='0 0 24 24'
-							strokeWidth='1.5'
-							stroke='currentColor'
-							fill='none'
-							strokeLinecap='round'
-							strokeLinejoin='round'
-						>
-							<path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
-							<path d='M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2'></path>
-							<path d='M9 12h12l-3 -3'></path>
-							<path d='M18 15l3 -3'></path>
-						</svg>
-						Cerrar sesión
-					</button>
+			<div aria-disabled className='w-[732px] -mb-[366px] relative -left-[200vw]'>
+				<div id='ticket' className='border-[16px] border-transparent'>
+					<TicketComponent
+						isSizeFixed
+						number={ticketNumber}
+						flavor={flavor}
+						user={{ username, avatar, name }}
+					/>
 				</div>
-
-				<div className='max-w-[700px] mx-auto'>
-					<Container3D>
-						<TicketComponent number={number} flavor={flavor} user={{ username, avatar, name }} />
-					</Container3D>
-				</div>
-				<div aria-disabled className='w-[732px] -mb-[366px] relative -left-[200vw]'>
-					<div id='ticket' className='border-[16px] border-transparent'>
-						<TicketComponent
-							isSizeFixed
-							number={number}
-							flavor={flavor}
-							user={{ username, avatar, name }}
-						/>
+			</div>
+			<main
+				className={`${GeistSans.className} max-w-screen-xl m-auto mt-40 pb-20 gap-8 px-4 lg:grid grid-cols-[auto_1fr] items-center`}
+			>
+				<div>
+					<div className='w-auto'>
+						<div className='max-w-[400px] md:max-w-[700px] mx-auto'>
+							<Container3D>
+								<TicketComponent
+									number={ticketNumber}
+									flavor={flavor}
+									user={{ username, avatar, name }}
+								/>
+							</Container3D>
+						</div>
+						<div
+							className={cn(
+								'w-2/3 mx-auto h-6 rounded-[50%] mt-6 transition-colors duration-300 blur-xl',
+								flavor.colorPalette?.bg
+							)}
+						></div>
+					</div>
+					<div className='flex flex-col items-center justify-between w-full px-16 m-auto mt-16 mb-16 text-center lg:mb-0 lg:mt-4 md:flex-row'>
+						<button
+							onClick={handleShare}
+							type='button'
+							className={`text-white cursor-pointer hover:bg- bg-[#14171A] hover:bg-[#14171A]/90 focus:ring-4 focus:outline-none focus:ring-[#14171A]/50 font-medium rounded-lg px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#14171A]/55 mr-2 mb-2 hover:shadow-lg transition-all duration-200 ease-in-out border border-white/40 hover:scale-125 [text-wrap:balance] ${
+								buttonText !== STEPS_LOADING.ready
+									? 'pointer-events-none opacity-70 bg-slate-700'
+									: ''
+							}`}
+						>
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								className='w-4 h-4 mr-2'
+								width='1200'
+								height='1227'
+								fill='none'
+								viewBox='0 0 1200 1227'
+							>
+								<path
+									fill='#fff'
+									d='M714.163 519.284 1160.89 0h-105.86L667.137 450.887 357.328 0H0l468.492 681.821L0 1226.37h105.866l409.625-476.152 327.181 476.152H1200L714.137 519.284h.026ZM569.165 687.828l-47.468-67.894-377.686-540.24h162.604l304.797 435.991 47.468 67.894 396.2 566.721H892.476L569.165 687.854v-.026Z'
+								/>
+							</svg>
+							{buttonText}
+						</button>
+						<button
+							onClick={handleLogout}
+							className='flex-row justify-center  text-white cursor-pointer hover:bg-slate-700 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55 mr-2 mb-2 hover:shadow-lg transition-all duration-200 ease-in-out hover:scale-110 scale-90 gap-x-2 opacity-70 hover:opacity-100'
+							href='/'
+						>
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								width='24'
+								height='24'
+								viewBox='0 0 24 24'
+								strokeWidth='1.5'
+								stroke='currentColor'
+								fill='none'
+								strokeLinecap='round'
+								strokeLinejoin='round'
+							>
+								<path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+								<path d='M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2'></path>
+								<path d='M9 12h12l-3 -3'></path>
+								<path d='M18 15l3 -3'></path>
+							</svg>
+							Cerrar sesión
+						</button>
 					</div>
 				</div>
-
-				<div className='w-full z-[99999] opacity-[.99] mt-10 md:mt-2'>
-					<h2 className='font-light text-center text-white uppercase opacity-70'>
-						Selecciona tu sabor:
-					</h2>
-					<div className='flex flex-row justify-center w-full p-8 overflow-x-auto text-center flex-nowrap md:flex-wrap gap-x-8 gap-y-12'>
-						{Object.entries(FLAVORS).map(([key, { icon: Icon }]) => {
-							return (
-								<button
-									key={key}
-									className={`relative flex w-16 h-16 transition cursor:pointer group ${
-										key === flavorKey
-											? 'scale-125 pointer-events-none contrast-125 before:absolute before:rounded-full before:w-2 before:h-2 before:left-0 before:right-0 before:-top-4 before:mx-auto before:bg-yellow-200'
-											: ''
-									}`}
-									onClick={changeFlavorKey(key)}
-								>
-									<div className='flex items-center justify-center w-16 h-16 transition group-hover:scale-110'>
-										<Icon className='h-auto' />
-									</div>
-								</button>
-							)
-						})}
+				<div>
+					<div>
+						<h2 className='text-2xl font-bold text-white lg:pl-8'>Material</h2>
+						<div className='flex flex-wrap items-center px-8 py-3 gap-x-6 gap-y-2'>
+							<Button className='py-1' variant='secondary'>
+								Estándar{' '}
+								<span className='px-2 py-1 text-xs rounded-full bg-white/10 text-white/60'>
+									Seleccionado
+								</span>
+							</Button>
+							<p className='text-sm text-white/60'>¡Próximamente nuevos materiales!</p>
+						</div>
+					</div>
+					<div className='w-full z-[99999] opacity-[.99] mt-10 md:mt-2'>
+						<h2 className='text-2xl font-bold text-white lg:pl-8'>Tecnología</h2>
+						<div className='flex flex-row w-full p-8 max-h-[30rem] overflow-x-auto text-center flex-nowrap md:flex-wrap gap-x-8 gap-y-12 lg:pb-20 hidden-scroll lg:flavors-gradient-list'>
+							{Object.entries(FLAVORS).map(([key, { icon: Icon }]) => {
+								return (
+									<button
+										key={key}
+										className={`relative flex w-12 h-12 transition cursor:pointer group ${
+											key === flavorKey
+												? 'scale-125 pointer-events-none contrast-125 before:absolute before:rounded-full before:w-2 before:h-2 before:left-0 before:right-0 before:-top-4 before:mx-auto before:bg-yellow-200'
+												: ''
+										}`}
+										onClick={changeFlavorKey(key)}
+									>
+										<div className='flex items-center justify-center w-12 h-12 transition group-hover:scale-110'>
+											<Icon className='h-auto' />
+										</div>
+									</button>
+								)
+							})}
+						</div>
 					</div>
 				</div>
 			</main>
 		</Layout>
 	)
+}
+
+const PREFIX_CDN = 'https://ljizvfycxyxnupniyyxb.supabase.co/storage/v1/object/public/tickets'
+
+async function dataUrlToFile(dataUrl, fileName) {
+	const res = await fetch(dataUrl)
+	const blob = await res.blob()
+	return new File([blob], fileName, { type: 'image/jpg' })
+}
+
+const STEPS_LOADING = {
+	ready: 'Compartir ticket en X',
+	generate: 'Generando nuevo ticket...',
+	sharing: 'Compartiendo ticket...'
+}
+
+const getInfoFromUser = ({ user }) => {
+	const { user_metadata: meta } = user
+	const { avatar_url: avatar, full_name: fullname, preferred_username: username } = meta
+
+	return { avatar, fullname, username }
 }
 
 export const getServerSideProps = async (ctx) => {
