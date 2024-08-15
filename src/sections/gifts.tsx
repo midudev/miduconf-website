10694e4719a7eefafc3041e500f4f946e1feb6fe
madeval <alvarez.fing@gmail.com>
2024-08-15 17:57:48 -0300

    feat: create hidden gift section

--- src/sections/gifts.tsx
+++ src/sections/gifts.tsx
@@ -1,5 +1,7 @@
 import { HideContentBox } from '@/components/HideContentBox'
 import { NavbarIcons } from '@/components/icons/navbar'
+import { cn } from '@/lib/utils'
+import { useSupabaseClient } from '@supabase/auth-helpers-react'
 
 const GIFTS = [
 	{
@@ -95,54 +97,129 @@ export const Gifts = () => {
 				subtitle='¡Muy pronto desvelaremos todos los regalos!'
 				BgIcon={NavbarIcons.GiftIcon}
 			/>
+			{/* <GiftsSection /> */}
 		</section>
 	)
 }
 
 const GiftsSection = () => {
+	const supabase = useSupabaseClient()
+
+	const handleLogin = async () => {
+		const { data, error } = await supabase.auth.signInWithOAuth({
+			provider: 'github',
+			options: {
+				redirectTo:
+					process.env.NODE_ENV !== 'production'
+						? 'http://localhost:3000/api/auth/callback'
+						: 'https://miduconf.com/api/auth/callback'
+			}
+		})
+	}
+
 	return (
-		<section id='regalos' className='flex flex-col flex-wrap items-center justify-center pt-24'>
-			<h2 className='text-6xl font-bold text-center text-white'>Regalos</h2>
-			<p className='max-w-xl text-xl text-sky-200 text-center [text-wrap:balance] mt-4'>
-				256 regalos para todo el mundo.
-				<br />
-				¡Necesitas tener{' '}
-				<a href='#' className='text-yellow-200 underline'>
-					el ticket gratuito para participar
-				</a>
-				!
-				<br />
-			</p>
+		<section
+			id='regalos'
+			className='pt-24 bg-button shadow-button rounded-[40px] max-w-screen-xl mx-auto px-4'
+		>
+			<h2 className='text-4xl font-bold text-center text-white sm:text-6xl'>
+				Regalos para la <span className='text-midu-primary'>comunidad</span>
+			</h2>
+			<p className='mt-4 text-lg text-center text-white/80'>+256 regalos para todo el mundo.</p>
+			<button
+				onClick={handleLogin}
+				className='block mx-auto text-lg text-center text-white underline'
+			>
+				¡Solo necesitas tener un ticket para participar!
+			</button>
 
-			<div className='bg-[#000214] group-hover:bg-[#000214]/10 w-full px-6 py-10 rounded transition flex flex-col gap-2 max-w-xl m-auto gap-y-4'>
-				{GIFTS.map((gift, index) => (
-					<div key={index} className='flex flex-col transition cursor-crosshair hover:scale-110'>
-						{gift.img && (
-							<img
-								className='object-cover w-full h-auto transition group-hover:scale-110'
-								src={`/img/gifts/${gift.img}.png`}
-								alt={`gift: ${gift.name}`}
-							/>
-						)}
-
-						<h5
-							className={`
-							${gift.special === 'super' && 'text-yellow-300 text-3xl'}
-							${gift.special === 'high' && 'text-yellow-200 text-2xl'}
-							${gift.special === undefined && 'text-yellow-100 text-xl'}
-						mb-2 text-2xl font-semibold tracking-tight
-						`}
-						>
-							{gift.count}x {gift.name}
-						</h5>
-						{gift.detail && <p className='-mt-2 text-sm text-sky-200 opacity-90'>{gift.detail}</p>}
-					</div>
+			<div className='grid grid-cols-1 mx-auto sm:grid-cols-2 lg:grid-cols-3 gap-7 my-11 max-w-screen-base'>
+				{LIST_OF_GIFTS.map((gift, index) => (
+					<GiftItem key={`${gift.title}-${index}`} {...gift} />
 				))}
 			</div>
-			<small className='text-center text-white text-sm max-w-xl opacity-80 [text-wrap:balance] mt-3'>
+
+			<small className='text-center w-auto mx-auto block text-white text-sm max-w-xl opacity-80 [text-wrap:balance] py-8'>
 				Si ganas un sorteo y no se puede enviar a tu país el regalo, buscaremos una alternativa del
 				mismo valor monetario.
 			</small>
 		</section>
+	)
+}
+
+const LIST_OF_GIFTS = [
+	{
+		imgUrl: '/regalo-libro-git.png',
+		imgSmallUrl: '/regalo-libro-git.png',
+		title: 'Ordenador Mac Mini'
+	},
+	{
+		imgUrl: '/regalo-libro-git.png',
+		imgSmallUrl: '/regalo-libro-git.png',
+		title: 'Ordenador Mac Mini Ordenador Mac Mini Ordenador Mac Mini'
+	},
+	{
+		imgUrl: '/regalo-libro-git.png',
+		imgSmallUrl: '/regalo-libro-git.png',
+		title: 'Ordenador Mac Mini'
+	},
+	{
+		imgUrl: '/regalo-libro-git.png',
+		imgSmallUrl: '/regalo-libro-git.png',
+		title: 'Ordenador Mac Mini'
+	},
+	{
+		imgUrl: '/regalo-libro-git.png',
+		imgSmallUrl: '/regalo-libro-git.png',
+		title: 'Ordenador Mac Mini Ordenador Mac Mini Ordenador Mac Mini'
+	},
+	{
+		imgUrl: '/regalo-libro-git.png',
+		imgSmallUrl: '/regalo-libro-git.png',
+		title: 'Ordenador Mac Mini'
+	}
+]
+
+interface GiftItemProps {
+	imgUrl: string
+	imgSmallUrl: string
+	title: string
+	className?: string
+	level?: 1 | 2 | 3
+}
+
+function GiftItem({ imgUrl, title, imgSmallUrl, className, level = 1 }: GiftItemProps) {
+	return (
+		<div
+			className={cn(
+				'relative flex items-center justify-center py-5 transition-all duration-500 border bg-button rounded-2xl group px-7 before:w-full before:h-full before:rounded-2xl before:backdrop-blur-md before:z-10 before:absolute before:inset-0 hover:before:backdrop-blur-none cursor-crosshair',
+				className,
+				level === 1 && 'bg-midu-primary/30 border-midu-primary',
+				level === 2 && 'bg-[#5C9AFF]/30 border-[#5C9AFF]',
+				level === 3 && 'bg-[#93C5FD]/30 border-[#93C5FD]'
+			)}
+		>
+			<p
+				className={cn(
+					'absolute z-20 w-full text-lg font-bold text-center text-white -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2',
+					' group-hover:translate-y-1/2 transition-all duration-500'
+				)}
+			>
+				{title}
+			</p>
+			<div
+				className={cn(
+					'relative h-auto opacity-20 w-max rotate-6',
+					'group-hover:-translate-y-1/3 transition-all duration-500 group-hover:opacity-100'
+				)}
+			>
+				<img src={imgUrl} className='w-32 h-auto drop-shadow-gift' alt='' />
+				<img
+					src={imgSmallUrl}
+					className='w-16 h-auto drop-shadow-gift absolute -bottom-0.5 -right-2'
+					alt=''
+				/>
+			</div>
+		</div>
 	)
 }
