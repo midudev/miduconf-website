7e87789e17cfa34ac49f11619a9ebf415d253f97
christian_reynaltt <creynaltt@maristak.net>
2024-08-27 18:47:28 +0200

    Fix: Make Country span tag is readable

--- src/components/Speakers.tsx
+++ src/components/Speakers.tsx
@@ -108,7 +108,7 @@ function Speaker({ name, title, twitter, img, country }) {
 				</header>
 				<footer className='flex items-center justify-between gap-x-2'>
 					<p className='text-xs text-left text-white/60'>{title}</p>
-					<span>{country}</span>
+					<span className='text-white'>{country}</span>
 				</footer>
 			</div>
 		</article>
