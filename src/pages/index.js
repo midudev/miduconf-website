41841a61948061e2a3dbabdbb3dcb1ae3f494794
Miguel Ángel Durán <miduga@gmail.com>
2024-09-19 11:38:10 +0200

    Add JSConf

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import { GeistSans } from 'geist/font/sans'
 
 import { Background } from '@/components/Background'
+import { TicketIcon } from '@/components/icons'
+import { Button } from '@/components/Button'
 import { Meteors } from '@/components/MeteorLanguages'
 import { Speakers } from '@/components/Speakers'
 import { Stars } from '@/components/Stars'
@@ -47,10 +49,29 @@ export default function Home({
 					<Meteors />
 					<Background />
 					<div className='max-w-5xl mx-auto'>
-						<h2 className='animate-fade-in-up text-5xl sm:text-6xl md:text-[80px] mx-auto text-center max-w-[20ch] text-white font-bold pt-40'>
-							Conoce el <span className='text-midu-primary'>futuro</span> de la{' '}
-							<span className='text-midu-primary'>programación</span>
+						<h2 className='animate-fade-in-up text-5xl sm:text-6xl md:text-[80px] mx-auto text-center max-w-[20ch] text-white font-bold pt-60 pb-4 text-balance'>
+							¡Gracias por disfrutar la <span className='text-yellow-300'>miduConf 2024</span>!
 						</h2>
+						<h3 className='text-white text-lg text-center pb-4 pt-4'>
+							Volvemos en el año que viene con más y mejores charlas y sorteos.
+						</h3>
+						<h3 className='text-yellow-300 text-lg text-center pb-4'>
+							Pero el 1 de marzo de 2025 tenemos nueva conferencia:
+						</h3>
+
+						<div className='flex justify-center'>
+							<Button
+								as='a'
+								href='https://jsconf.es'
+								className='w-content px-6 py-5 flex text-lg font-bold md:text-3xl rounded-xl bg-yellow-300/90 text-black shadow-yellow-400 hover:shadow-yellow-300 hover:bg-yellow-300 hover:contrast-125'
+							>
+								<TicketIcon className='mr-3' />
+								Ir a la JSConf España 2025
+							</Button>
+						</div>
+
+						<hr className='pt-8 mt-16' />
+
 						<TicketHome
 							ticketNumber={ticketNumber}
 							initialFlavor={flavor}
