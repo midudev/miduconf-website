9e46e7206c566d62474a367ec8b03377b029abeb
madeval <alvarez.fing@gmail.com>
2025-07-08 14:31:40 -0300

    refactor: format code

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,6 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
-export default function Document () {
+export default function Document() {
 	return (
 		<Html lang='es-ES'>
 			<Head />
