4726a047c16af53e2dfb399623cdf16f5cd952b9
madeval <alvarez.fing@gmail.com>
2025-07-09 18:23:20 -0300

    feat: improve sponsors and add responsive

--- src/sections/sponsors.tsx
+++ src/sections/sponsors.tsx
@@ -1,9 +1,59 @@
 import { Button } from '@/components/Button'
+import { DiamondIcon } from '@/components/icons/diamond'
 import { DotIcon } from '@/components/icons/dot'
 import { EnterArrow } from '@/components/icons/enter-arrow'
 import { SponsorIcons } from '@/components/icons/sponsors'
 import { cn } from '@/lib/utils'
 import Link from 'next/link'
+
+export const Sponsors = () => {
+	return (
+		<section id='sponsors' className='pt-44 grid md:grid-cols-[1fr_auto] px-8 gap-8'>
+			<div className='h-full'>
+				<header className='flex flex-col gap-2 max-w-80 md:col-[2/3] md:row-[1/2] mt-8 sticky top-40'>
+					<h2 className='flex items-center justify-center gap-4 text-4xl font-bold text-white uppercase'>
+						<DotIcon className='text-pallet-primary' /> Sponsors{' '}
+						<DotIcon className='text-pallet-primary' />
+					</h2>
+					<p className='mx-auto text-xl text-white text-pretty max-w-[42ch] text-center'>
+						Gracias a ellos hacemos posible el evento
+					</p>
+					<Link
+						href='mailto:miduga@gmail.com'
+						className='w-full flex mt-4 flex-col items-center gap-x-2 py-2.5 px-4 text-base text-white bg-pallet-primary uppercase rounded-md disabled:cursor-not-allowed md:flex-row justify-center'
+					>
+						<EnterArrow className='hidden w-auto h-3 md:block' />
+						Ser Patrocinador
+					</Link>
+				</header>
+			</div>
+
+			<div className='md:col-[1/2] md:row-[1/2]'>
+				<h3 className='flex items-center justify-center gap-2 mb-4 text-2xl font-bold text-white uppercase md:justify-start'>
+					<DiamondIcon className='w-auto h-4 text-yellow-200' />
+					Premium
+					<DiamondIcon className='w-auto h-4 text-yellow-200' />
+				</h3>
+				<div className='grid gap-5 md:grid-cols-2'>
+					{premiums.map((premium) => (
+						<ReviewCard className='max-w-80' key={premium.name} {...premium} size='large' />
+					))}
+				</div>
+
+				<h3 className='flex items-center justify-center gap-2 mt-12 mb-4 text-2xl font-bold text-white uppercase md:justify-start'>
+					<DotIcon className='w-auto h-4 text-pallet-primary' />
+					Gold
+					<DotIcon className='w-auto h-4 text-pallet-primary' />
+				</h3>
+				<div className='grid gap-5 md:grid-cols-3'>
+					{sponsors.map((sponsor) => (
+						<ReviewCard key={sponsor.name} className='py-4' {...sponsor} />
+					))}
+				</div>
+			</div>
+		</section>
+	)
+}
 
 const ReviewCard = ({
 	logo,
@@ -23,7 +73,11 @@ const ReviewCard = ({
 			href={link}
 			target='_blank'
 			rel='noopener noreferrer'
-			className='flex flex-col items-center justify-center gap-4 border rounded-md min-h-60 bg-pallet-b-foreground-primary border-pallet-border-foreground'
+			className={cn(
+				'flex flex-col items-center justify-center gap-4 border rounded-md min-h-60 bg-pallet-b-foreground-primary border-pallet-border-foreground',
+				'hover:bg-pallet-border-foreground hover:border-pallet-ghost transition',
+				'focus-visible:outline focus-visible:outline-white focus-visible:bg-pallet-border-foreground focus-visible:border-pallet-ghost'
+			)}
 		>
 			<figure className={`${size === 'large' ? 'h-12' : 'h-8'} flex items-center justify-center`}>
 				{logo}
@@ -36,45 +90,6 @@ const ReviewCard = ({
 				{slogan}
 			</p>
 		</a>
-	)
-}
-
-export const Sponsors = () => {
-	return (
-		<section id='sponsors' className='pt-44 grid grid-cols-[1fr_auto] px-4 gap-8'>
-			<header className='flex flex-col gap-2 max-w-80 col-[2/3] row-[1/2]'>
-				<h2 className='flex items-center justify-center gap-4 text-4xl font-bold text-white uppercase'>
-					<DotIcon className='text-pallet-primary' /> Sponsors{' '}
-					<DotIcon className='text-pallet-primary' />
-				</h2>
-				<p className='mx-auto text-xl text-white text-pretty max-w-[42ch] text-center'>
-					Gracias a ellos hacemos posible el evento
-				</p>
-				<Link
-					href='mailto:miduga@gmail.com'
-					className='w-full flex mt-4 flex-col items-center gap-x-2 py-2.5 px-4 text-base text-white bg-pallet-primary uppercase rounded-md disabled:cursor-not-allowed md:flex-row justify-center'
-				>
-					<EnterArrow className='hidden w-auto h-3 md:block' />
-					Ser Patrocinador
-				</Link>
-			</header>
-
-			<div className='col-[1/2] row-[1/2]'>
-				<h3 className='mb-10 text-2xl font-bold text-white'>Premium</h3>
-				<div className='grid grid-cols-2 gap-5'>
-					{premiums.map((premium) => (
-						<ReviewCard className='max-w-80' key={premium.name} {...premium} size='large' />
-					))}
-				</div>
-
-				<h3 className='mt-12 mb-10 text-2xl font-bold text-white'>Gold</h3>
-				<div className='grid grid-cols-2 gap-5'>
-					{sponsors.map((sponsor) => (
-						<ReviewCard key={sponsor.name} className='py-4' {...sponsor} />
-					))}
-				</div>
-			</div>
-		</section>
 	)
 }
 
