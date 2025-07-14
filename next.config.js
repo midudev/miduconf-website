4218832344e31196046b0462f3ebb4a11befbb40
midudev <miduga@gmail.com>
2025-07-14 11:03:13 +0200

    chore: eliminar configuración experimental y paquetes de transpilación en next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['geist'],
-  experimental: {
-    esmExternals: false // THIS IS THE FLAG THAT MATTERS
-  },
   async rewrites() {
     return [
       {
