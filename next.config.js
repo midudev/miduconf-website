e449bbb1b5d96d94a65304827f21b0d89589162c
midudev <miduga@gmail.com>
2025-07-14 10:57:08 +0200

    chore: formatear el archivo de configuración de Next.js para mejorar la legibilidad

--- next.config.js
+++ next.config.js
@@ -1,22 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	transpilePackages: ['geist'],
-	experimental: {
-		esmExternals: false // THIS IS THE FLAG THAT MATTERS
-	},
-	async rewrites() {
-		return [
-			{
-				source: '/ticket/:username',
-				destination: '/?ticket=:username'
-			},
-			{
-				source: '/ticket/:username/:hash',
-				destination: '/?ticket=:username'
-			}
-		]
-	}
+  reactStrictMode: true,
+  transpilePackages: ['geist'],
+  experimental: {
+    esmExternals: false // THIS IS THE FLAG THAT MATTERS
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/ticket/:username',
+        destination: '/?ticket=:username'
+      },
+      {
+        source: '/ticket/:username/:hash',
+        destination: '/?ticket=:username'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
