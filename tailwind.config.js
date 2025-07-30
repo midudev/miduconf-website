cc4b0c7eb622784ca2d31014d8ca30976afc2265
HeJimenez00 <hejimenez00@gmail.com>
2025-07-30 14:48:28 -0600

    chore: update custom spacing keys in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,22 +12,22 @@ module.exports = {
   theme: {
     extend: {
       imageRendering: {
-        pixelated: 'pixelated',
+        pixelated: 'pixelated'
       },
       spacing: {
-        8: '0.5rem',
-        16: '1rem',
-        24: '1.5rem',
-        32: '2rem',
-        40: '2.5rem',
-        64: '4rem',
-        96: '6rem',
-        180: '11.25rem'
+        'spacing-8': '0.5rem',
+        'spacing-16': '1rem',
+        'spacing-24': '1.5rem',
+        'spacing-32': '2rem',
+        'spacing-40': '2.5rem',
+        'spacing-64': '4rem',
+        'spacing-96': '6rem',
+        'spacing-180': '11.25rem'
       },
       fontFamily: {
-				sans: ['"Inter", sans-serif'],
-				code: ['code-regular', 'monospace'],
-				cta: ['code-medium', 'monospace'],
+        sans: ['"Inter", sans-serif'],
+        code: ['code-regular', 'monospace'],
+        cta: ['code-medium', 'monospace']
       },
       dropShadow: {
         gift: '3px 4px 8px rgba(0, 0, 0, 0.4)'
