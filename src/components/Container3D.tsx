6fe020df1e6e7d62892ecb756a67a20723365f8b
madeval <alvarez.fing@gmail.com>
2025-08-03 21:27:00 -0300

    feat: change rounded

--- src/components/Container3D.tsx
+++ src/components/Container3D.tsx
@@ -3,44 +3,44 @@ import { useEffect, useState } from 'react'
 import 'atropos/css'
 
 interface Props {
-	children: React.ReactNode
+  children: React.ReactNode
 }
 
 export const Container3D = ({ children }: Props) => {
-	const [isMobile, setIsMobile] = useState(false)
+  const [isMobile, setIsMobile] = useState(false)
 
-	useEffect(() => {
-		const mediaQuery = window.matchMedia('(max-width: 768px)')
-		setIsMobile(mediaQuery.matches)
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 768px)')
+    setIsMobile(mediaQuery.matches)
 
-		const handleMediaQueryChange = (e) => {
-			setIsMobile(e.matches)
-		}
+    const handleMediaQueryChange = (e) => {
+      setIsMobile(e.matches)
+    }
 
-		mediaQuery.addEventListener('change', handleMediaQueryChange)
+    mediaQuery.addEventListener('change', handleMediaQueryChange)
 
-		return () => {
-			mediaQuery.removeEventListener('change', handleMediaQueryChange)
-		}
-	}, [])
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+  }, [])
 
-	return (
-		<div className='relative z-[1000] w-full h-auto mx-auto'>
-			<div className='opacity-100 h-max isolate'>
-				<div className='flex items-center justify-center h-max'>
-					{isMobile ? (
-						<>{children}</>
-					) : (
-						<Atropos
-							highlight={true}
-							innerClassName='backdrop-blur-xl rounded-[17px]'
-							className='block w-max h-auto mx-auto shadow-2xl [box-sizing:border-box]'
-						>
-							{children}
-						</Atropos>
-					)}
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className='relative z-[1000] w-full h-auto mx-auto'>
+      <div className='opacity-100 h-max isolate'>
+        <div className='flex items-center justify-center h-max'>
+          {isMobile ? (
+            <>{children}</>
+          ) : (
+            <Atropos
+              highlight={true}
+              innerClassName='backdrop-blur-xl rounded-xl'
+              className='block w-max h-auto mx-auto shadow-2xl [box-sizing:border-box]'
+            >
+              {children}
+            </Atropos>
+          )}
+        </div>
+      </div>
+    </div>
+  )
 }
