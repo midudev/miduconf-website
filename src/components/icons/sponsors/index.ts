dfc10012b9ae3c789d2a18a4df588de9fbfe4d8c
madeval <alvarez.fing@gmail.com>
2025-08-06 13:56:53 -0300

    feat: add new premium sponsor

--- src/components/icons/sponsors/index.ts
+++ src/components/icons/sponsors/index.ts
@@ -4,11 +4,13 @@ import { LemonCodeVerticalIcon } from './lemon-vertical'
 import { MaltIcon } from './malt'
 import { CodelyIcon } from './codely'
 import { PlainConcepts } from './plain-concepts'
+import { IgniaCLoudIcon } from './ignia-cloud'
 
 export const SponsorIcons = {
   cloudinary: CloudinaryIcon,
   lemonCodeHorizontal: LemonCodeHorizontalIcon,
   lemonCodeVertical: LemonCodeVerticalIcon,
+  igniaCloud: IgniaCLoudIcon,
   malt: MaltIcon,
   codely: CodelyIcon,
   plainConcepts: PlainConcepts
