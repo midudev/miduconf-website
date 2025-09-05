204f583682598dd93958a98b8cee26d021138fa0
midudev <miduga@gmail.com>
2025-09-05 12:07:04 +0200

    feat: add meta tags and preconnect links for performance optimization

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,13 +1,22 @@
 import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
-	return (
-		<Html lang='es-ES'>
-			<Head />
-			<body>
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	)
+  return (
+    <Html lang='es-ES'>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='theme-color' content='#09090E' />
+        <link
+          rel='preconnect'
+          href='https://ljizvfycxyxnupniyyxb.supabase.co'
+          crossOrigin='anonymous'
+        />
+        <link rel='dns-prefetch' href='//ljizvfycxyxnupniyyxb.supabase.co' />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
